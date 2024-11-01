2d4e3c47253e3d088d027746c7ebde163da86272
Schimmeltoast08 <126913128+Schimmeltoast08@users.noreply.github.com>
2024-11-01 22:25:24 +0100

    completely remade the enemie
    i completely remade the enemie so it can be instanciated and doesn't run on leetcode :3 it works with custom signals now instead of relying on a double line variable from saba, which I still don't understand

--- project-snek/scenes/basic_robot.gd
+++ project-snek/scenes/basic_robot.gd
@@ -1,30 +1,18 @@
-extends Sprite2D
+extends CharacterBody2D
 
-var movement_speed = 1
-var direction:int = 1
-var start_position
-var end_position
+signal player_near_robot
+signal player_near_robot_exited
 
-func _ready() -> void:
-	start_position = int($"../start_position".position.x)
-	end_position = int($"../end_position".position.x)
-	position.x = start_position
-	position.y = $"../end_position".position.y
+var init_count = 1
 
 
 
-func _process(delta: float) -> void:
-	#calculate if main character is near to robot
-	var near = (position.x - $"../../CharacterBody2D".position.x < 100 && position.x - $"../../CharacterBody2D".position.x > -100 &&
-		position.y - $"../../CharacterBody2D".position.y < 150 && position.y - $"../../CharacterBody2D".position.y > -150)
-	# /calculate if main character is near to robot\
-	
-	if(position.x == start_position): direction = 1
-	elif(position.x == end_position): direction = -1
-	
-	if  !(Input.is_action_pressed("button_e") && near):
-		if(direction == 1): position.x +=1	
-		else: position.x -=1	
-			
-	if near: $border.visible = true
-	else: $border.visible = false
+
+
+func _on_area_near_body_entered(body):
+	player_near_robot.emit()
+	$border.visible = true
+
+func _on_area_near_body_exited(body):
+	player_near_robot_exited.emit()
+	$border.visible = false
---
+++ project-snek/scenes/basic_robot_revamped.gd
@@ -0,0 +1,13 @@
+extends CharacterBody2D
+
+
+export (String, "loop", "linear") var patrol_type = "linear"
+
+
+func _ready():
+	pass 
+
+
+
+func _process(delta):
+	pass	
