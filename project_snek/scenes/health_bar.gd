72db0e2e0a26146a47d854941c72f55382a38074
Schimmeltoast08 <126913128+Schimmeltoast08@users.noreply.github.com>
2024-12-25 20:54:23 +0100

    added global preload var
    added global preload var

--- project_snek/scenes/health_bar.gd
+++ project_snek/scenes/health_bar.gd
@@ -5,12 +5,24 @@ extends ProgressBar
 
 func _ready() -> void:
 	value = 10
- 
+	var sb = StyleBoxFlat.new()
+	add_theme_stylebox_override("fill", sb)
+	sb.bg_color = Color("ff0000") 
+
+func _process(delta: float) -> void:
+	#value = move_toward(value,Player.Health,0.2)
+	pass
+
 func _on_character_body_2d_player_health_changed(Health, maximum_player_health):
 	
-	# because the health gets sent here before it gets taken away from the player, this takes it away here too.
-	# i know this is spaghetti code but i gotta go but don't want to leave this not working rn. // Toasti :3
-	Health -= 1
 	
-	# value is the value of the progress bar
+	Health -= 1
 	value = Health
+	#var New_Health = Health - 1
+	
+	# //TODO: Make the health bar smooth. I've been trying for 1h and can't do it. //TODO
+	# my only issue: the _process function has no access to the variable Health of the _on_charac... func
+	
+	#value = move_toward(Health,New_Health,0.2)
+	# value is the value of the progress bar
+	
