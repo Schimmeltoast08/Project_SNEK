a18b94143c632cd984b684bf08711848a447da90
Schimmeltoast08 <Scherer.bastian308@gmail.com>
2025-01-06 22:51:53 +0100

    fixed 3 bugs
    fixed the bug with the way too low gravity when teleporting upwards, the bug with too low gravity when standing on top of an enemy and the bug where the player looses health at the beginning of the game

--- project_snek/scenes/global_vars.gd
+++ project_snek/scenes/global_vars.gd
@@ -1,2 +1,18 @@
 extends Node
-# for global variables
+### for global variables
+
+var enemy_near = false
+var tmp_fix_early_trigger_bug = 0 # for some reason the node sends the "take damage" signal
+#									at the start of the game, this just prevents this
+
+
+# this must be un-commented
+
+func _on_enemy_rootnode_player_near_robot() -> void:
+	if tmp_fix_early_trigger_bug > 0:
+		enemy_near = true
+	tmp_fix_early_trigger_bug += 1
+
+
+func _on_enemy_rootnode_player_near_robot_exited() -> void:
+	enemy_near = false
